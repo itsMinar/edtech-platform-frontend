cd335d77ac774684e7a4d7eeee9a7d5b7392a195
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-12-29 18:40:44 +0600

    Add Navbar to Student Layout

--- src/layouts/StudentLayout.jsx
+++ src/layouts/StudentLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Student/Navbar';
 
 export default function StudentLayout() {
   return (
     <>
-      {/* Navbar */}
+      <Navbar />
       <section className="py-6 bg-primary">
         <Outlet />
       </section>
