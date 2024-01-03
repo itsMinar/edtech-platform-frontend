a6aa65e4666f2a33fda642d9c6b62e7ef30dfd42
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-01-03 19:42:48 +0600

    Add Navbar to Admin Layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Admin/Navbar';
 
 export default function AdminLayout() {
   return (
     <>
-      {/* Navbar */}
+      <Navbar />
       <section className="py-6 bg-primary">
         <Outlet />
       </section>
