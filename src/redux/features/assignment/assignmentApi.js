4f7e508961667edc4e3a78fb69ce3e67c5cd4006
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-01-04 07:31:13 +0600

    Create Assignmnet Add, Edit and Delete functionality (Admin) in Redux

--- src/redux/features/assignment/assignmentApi.js
+++ src/redux/features/assignment/assignmentApi.js
@@ -5,7 +5,96 @@ export const assignmentsApi = apiSlice.injectEndpoints({
     getAssignments: builder.query({
       query: () => '/assignments',
     }),
+
+    addAssignment: builder.mutation({
+      query: (data) => ({
+        url: '/assignments',
+        method: 'POST',
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const assignment = await queryFulfilled;
+
+          dispatch(
+            apiSlice.util.updateQueryData(
+              'getAssignments',
+              undefined,
+              (draft) => {
+                draft.push(assignment.data);
+              }
+            )
+          );
+        } catch (error) {
+          console.log('Something went wrong!');
+        }
+      },
+    }),
+
+    editAssignment: builder.mutation({
+      query: ({ data, id }) => ({
+        url: `/assignments/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const assignment = await queryFulfilled;
+
+          dispatch(
+            apiSlice.util.updateQueryData(
+              'getAssignments',
+              undefined,
+              (draft) => {
+                const AssignmentIndex = draft?.findIndex(
+                  (t) => t?.id == assignment?.data?.id
+                );
+
+                draft[AssignmentIndex] = assignment?.data;
+              }
+            )
+          );
+        } catch (error) {
+          console.log('Something went wrong!');
+        }
+      },
+    }),
+
+    deleteAssignment: builder.mutation({
+      query: (id) => ({
+        url: `/assignments/${id}`,
+        method: 'DELETE',
+      }),
+      async onQueryStarted(id, { queryFulfilled, dispatch }) {
+        const deleteResult = dispatch(
+          apiSlice.util.updateQueryData(
+            'getAssignments',
+            undefined,
+            (draft) => {
+              const index = draft.findIndex((c) => c.id == id);
+
+              if (index !== -1) {
+                draft.splice(index, 1);
+              }
+            }
+          )
+        );
+
+        try {
+          await queryFulfilled;
+        } catch (err) {
+          deleteResult.undo();
+        }
+      },
+    }),
   }),
 });
 
-export const { useGetAssignmentsQuery } = assignmentsApi;
+export const {
+  useGetAssignmentsQuery,
+  useAddAssignmentMutation,
+  useEditAssignmentMutation,
+  useDeleteAssignmentMutation,
+} = assignmentsApi;
