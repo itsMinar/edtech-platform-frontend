2a3ce602a3b88df059d9a37681f6fa93f09a7d24
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-01-04 08:48:01 +0600

    Create Quiz Add, Edit and Delete functionality (Admin) in Redux

--- src/redux/features/quizzes/quizzesApi.js
+++ src/redux/features/quizzes/quizzesApi.js
@@ -5,7 +5,84 @@ export const quizzesApi = apiSlice.injectEndpoints({
     getQuizzes: builder.query({
       query: () => '/quizzes',
     }),
+
+    addQuizzes: builder.mutation({
+      query: (data) => ({
+        url: '/quizzes',
+        method: 'POST',
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const quiz = await queryFulfilled;
+
+          dispatch(
+            apiSlice.util.updateQueryData('getQuizzes', undefined, (draft) => {
+              draft.push(quiz.data);
+            })
+          );
+        } catch (error) {
+          console.log('Something went wrong!');
+        }
+      },
+    }),
+
+    editQuizzes: builder.mutation({
+      query: ({ data, id }) => ({
+        url: `/quizzes/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const quiz = await queryFulfilled;
+
+          dispatch(
+            apiSlice.util.updateQueryData('getQuizzes', undefined, (draft) => {
+              const QuizzesIndex = draft?.findIndex(
+                (t) => t?.id == quiz?.data?.id
+              );
+
+              draft[QuizzesIndex] = quiz?.data;
+            })
+          );
+        } catch (error) {
+          console.log('Something went wrong!');
+        }
+      },
+    }),
+
+    deleteQuizzes: builder.mutation({
+      query: (id) => ({
+        url: `/quizzes/${id}`,
+        method: 'DELETE',
+      }),
+      async onQueryStarted(id, { queryFulfilled, dispatch }) {
+        const deleteResult = dispatch(
+          apiSlice.util.updateQueryData('getQuizzes', undefined, (draft) => {
+            const index = draft.findIndex((c) => c.id == id);
+
+            if (index !== -1) {
+              draft.splice(index, 1);
+            }
+          })
+        );
+
+        try {
+          await queryFulfilled;
+        } catch (err) {
+          deleteResult.undo();
+        }
+      },
+    }),
   }),
 });
 
-export const { useGetQuizzesQuery } = quizzesApi;
+export const {
+  useGetQuizzesQuery,
+  useAddQuizzesMutation,
+  useEditQuizzesMutation,
+  useDeleteQuizzesMutation,
+} = quizzesApi;
