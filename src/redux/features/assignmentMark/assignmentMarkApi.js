97db17ea8751e6a659c63e35c06c0e0ce287278c
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-01-05 23:35:50 +0600

    Create Assignment Mark Edit functionality (Admin) in Redux

--- src/redux/features/assignmentMark/assignmentMarkApi.js
+++ src/redux/features/assignmentMark/assignmentMarkApi.js
@@ -31,8 +31,41 @@ export const assignmentMarkApi = apiSlice.injectEndpoints({
         }
       },
     }),
+
+    editAssignmentMark: builder.mutation({
+      query: ({ data, id }) => ({
+        url: `/assignmentMark/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const assignment = await queryFulfilled;
+
+          dispatch(
+            apiSlice.util.updateQueryData(
+              'getAssignmentMark',
+              undefined,
+              (draft) => {
+                const AssignmentMarkIndex = draft?.findIndex(
+                  (t) => t?.id == assignment?.data?.id
+                );
+
+                draft[AssignmentMarkIndex] = assignment?.data;
+              }
+            )
+          );
+        } catch (error) {
+          console.log('Something went wrong!');
+        }
+      },
+    }),
   }),
 });
 
-export const { useGetAssignmentMarkQuery, useAddAssignmentMarkMutation } =
-  assignmentMarkApi;
+export const {
+  useGetAssignmentMarkQuery,
+  useAddAssignmentMarkMutation,
+  useEditAssignmentMarkMutation,
+} = assignmentMarkApi;
